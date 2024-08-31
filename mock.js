720d30ca9492328819b3bd0ef80e12d80eca17ed
wisanu <wisanu@odds.team>
2024-08-31 16:19:59 +0700

    Mock data (javascritp file)

---
+++ mock.js
@@ -0,0 +1,38 @@
+const data = [
+    {
+      "Name": "John Doe",
+      "Email": "john@example.com",
+      "Joined": "Jan 21, 2020",
+      "Type": "New",
+      "Status": "Liked"
+    },
+    {
+      "Name": "Kaiden Goodwin",
+      "Email": "kaiden@example.com",
+      "Joined": "Jan 11, 2020",
+      "Type": "Member",
+      "Status": "Shared"
+    },
+    {
+      "Name": "Hanna Harris",
+      "Email": "hanna@example.com",
+      "Joined": "Jan 11, 2020",
+      "Type": "New",
+      "Status": "Shared"
+    },
+    {
+      "Name": "Magnus Yates",
+      "Email": "magnus@example.com",
+      "Joined": "Jan 10, 2020",
+      "Type": "Member",
+      "Status": "Shared"
+    },
+    {
+      "Name": "Sharon Price",
+      "Email": "sharon@example.com",
+      "Joined": "Jan 10, 2020",
+      "Type": "Member",
+      "Status": "Liked"
+    }
+  ]
+  
