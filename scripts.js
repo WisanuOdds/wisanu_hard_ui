3938959d059d850fa3c62a7ff45eb77ec9a6c61b
wisanu <wisanu@odds.team>
2024-08-31 13:47:29 +0700

    Add search and add scripts for animation  search  when I click hamberger icon

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 // JavaScript to toggle the sidebar and move the hamburger icon
 const sidebar = document.getElementById("sidebar");
 const hamburger = document.getElementById("hamburger");
+const search = document.getElementById("search");
 const mainContent = document.getElementById("main-content");
 
 hamburger.addEventListener("click", () => {
   sidebar.classList.toggle("active");
   hamburger.classList.toggle("active");
+  search.classList.toggle("active");
   mainContent.classList.toggle("shift");
 });
