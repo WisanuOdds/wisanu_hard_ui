16f329c65c55f43323cb74b942010282268394a1
wisanu <wisanu@odds.team>
2024-09-04 09:35:01 +0700

    Set up dark mode

--- scripts.js
+++ scripts.js
@@ -42,6 +42,27 @@ const hamburger = document.getElementById("hamburger");
 const search = document.getElementById("search");
 const mainContent = document.getElementById("main-content");
 const body = document.querySelector("body");
+let darkmode =localStorage.getItem("darkmode");
+const themeSwitch= document.getElementById("theme-switch");
+
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+}
+
+const disableDarkMode = () => {
+  document.body.classList.remove("darkmode");
+  localStorage.removeItem("darkmode",null);
+}
+
+if (darkmode === "active") {
+  enableDarkMode();
+}
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
+  darkmode !== "active" ? enableDarkMode() : disableDarkMode();
+});
 
 hamburger.addEventListener("click", () => {
   if (window.innerWidth > 825) {
